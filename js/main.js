186fe2d6730eba216f8e4ebb4d023b2b7adda328
Lucien Erard <erard@omega.localdomain>
2024-10-10 22:32:19 +0200

    The delay was modified.

--- js/main.js
+++ js/main.js
@@ -1,29 +1,31 @@
 'use strict';
 
 let container = document.querySelector('.container');
+const DURATION = 1200
+const DELAY = 200
+container.style.setProperty('--duration', `${DURATION}ms`)
 
 const text = 'Hello World!';
 
-const doing = setTimeout(
-    () => {
-        const addMsgs = setInterval(() => {
-            const MSG = document.createElement('div');
-            MSG.classList.add('msg');
-            MSG.classList.add('hidden');
-            const MSGCONTENT = document.createElement('p');
-            MSGCONTENT.innerHTML = text + ` ${container.childElementCount + 1}`;
-            setTimeout(() => {
-                MSG.classList.add('translated');
-            }, 4);
-            setTimeout(() => {
-                MSG.classList.add('showed');
-            }, 404);
-            MSG.appendChild(MSGCONTENT);
-            container.appendChild(MSG);
-            if (container.childElementCount === 5) {
-                clearInterval(addMsgs);
-                clearTimeout(doing);
-            }
-        }, 408);
-    }, 1000
-);
+function produceMessage() {
+    const addMsgs = setInterval(() => {
+        const MSG = document.createElement('div');
+        MSG.classList.add('msg');
+        MSG.classList.add('hidden');
+        const MSGCONTENT = document.createElement('p');
+        MSGCONTENT.innerHTML = text.toUpperCase() + ` ${container.childElementCount + 1}`;
+        setTimeout(() => {
+            MSG.classList.add('translated');
+        }, DELAY);
+        setTimeout(() => {
+            MSG.classList.add('showed');
+        }, DURATION);
+        MSG.appendChild(MSGCONTENT);
+        container.appendChild(MSG);
+        if (container.childElementCount === 5) {
+            clearInterval(addMsgs);
+        }
+    }, DURATION + DELAY);
+}
+
+produceMessage()
